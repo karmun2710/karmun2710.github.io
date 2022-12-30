86617e1003fb46b262b28b82c508f5f33eb38596
Hong Ming <pe21237@bristol.ac.uk>
2022-12-30 15:59:48 +0800

    Python Analysis

---
+++ python_analysis/constants.py
@@ -0,0 +1,13 @@
+states_idx = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+states = ['Johor', 'Kedah', 'Kelantan', 'Melaka', 'Negeri Sembilan', 'Pahang', 'Pulau Pinang', 'Perak', 'Perlis', 'Selangor',
+          'Terengganu', 'Sabah', 'Sarawak', 'W.P. Kuala Lumpur', 'W.P. Labuan', 'W.P. Putrajaya']
+
+states_short = ['JHR', 'KDH', 'KTN', 'MLK', 'NSN', 'PHG', 'PNG',
+                'PRK', 'PLS', 'SGR', 'TRG', 'SBH', 'SWK', 'KUL', 'LBN', 'PJY']
+
+idx2state = dict(zip(states_idx, states))
+
+state2idx = {v: k for k, v in idx2state.items()}
+
+LOCAL_DATA_SPR = 'D:/data_spr/'
